06967914cbbfed0e5658b8b86648eaa17c71e848
Pavo <brockbackups@gmail.com>
2019-06-30 13:51:22 -0400

    Added checks for tools required

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+check=$((xcode-\select --install) 2>&1)
+str="xcode-select: note: install requested for command line developer tools"
+
 prompt() {
   echo "$1"
   if [ "$FORCE_INSTALL" != "1" ]; then
@@ -10,9 +13,28 @@ prompt() {
   fi
 }
 
-if [ "$(which clang)" = "" ] || [ "$(which git)" = "" ] || [ "$(clang -v 2>&1 | grep "no developer")" != "" ] || [ "$(git -v 2>&1 | grep "no developer")" != "" ]; then
-  echo "Missing Xcode tools, please install them!"
-  exit 1
+while [[ "$check" == "$str" ]];
+do
+  osascript -e 'tell app "System Events" to display dialog "xcode command-line tools missing." buttons "OK" default button 1 with title "xcode command-line tools"'
+  exit;  
+done
+
+if [[ $(command -v brew) == "" ]]; then 
+		prompt "Homebrew is not installed. Would you like to install it now?"
+    pushd /tmp >/dev/null
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || exit 1
+    popd >/dev/null
+	else
+		echo "Homebrew is installed..."
+fi
+
+if command -v python3 &>/dev/null; then
+    echo "Python 3 is installed."
+else
+    prompt "Python 3 is not installed. Would you like to install it now?"
+    pushd /tmp >/dev/null
+    brew install python3 || exit 1
+    popd >/dev/null
 fi
 
 if [ "$(nasm -v)" = "" ] || [ "$(nasm -v | grep Apple)" != "" ]; then
@@ -47,76 +69,93 @@ if [ "$(which mtoc.NEW)" == "" ] || [ "$(which mtoc)" == "" ]; then
   popd >/dev/null
 fi
 
-cd /tmp
-mkdir BuildingAllTheShit
-cd BuildingAllTheShit
-echo "Cloning Vit9696's Repos."
-git clone https://github.com/acidanthera/Lilu.git > /dev/null 2>&1
-git clone https://github.com/acidanthera/WhateverGreen.git > /dev/null 2>&1
-git clone https://github.com/acidanthera/AppleALC.git > /dev/null 2>&1
-git clone https://github.com/acidanthera/VirtualSMC.git > /dev/null 2>&1
-git clone https://github.com/acidanthera/OpenCorePkg.git > /dev/null 2>&1
-git clone https://github.com/acidanthera/AptioFixPkg.git > /dev/null 2>&1
-git clone https://github.com/acidanthera/AppleSupportPkg.git > /dev/null 2>&1
-cd Lilu
+if [ -d "/tmp/BuildingAllTheShit" ]; then
+    echo "Removing current BuildingAllTheShit folder."
+    rm -rf /tmp/BuildingAllTheShit || exit 1
+    mkdir /tmp/BuildingAllTheShit || exit 1
+else
+    mkdir /tmp/BuildingAllTheShit || exit 1
+fi
+
+cd /tmp/BuildingAllTheShit
+echo "Cloning acidanthera's Repos."
+git clone https://github.com/acidanthera/Lilu.git > /dev/null 2>&1 || exit 1
+git clone https://github.com/acidanthera/WhateverGreen.git > /dev/null 2>&1 || exit 1
+git clone https://github.com/acidanthera/AppleALC.git > /dev/null 2>&1 || exit 1
+git clone https://github.com/acidanthera/VirtualSMC.git > /dev/null 2>&1 || exit 1
+git clone https://github.com/acidanthera/OpenCorePkg.git > /dev/null 2>&1 || exit 1
+git clone https://github.com/acidanthera/AptioFixPkg.git > /dev/null 2>&1 || exit 1
+git clone https://github.com/acidanthera/AppleSupportPkg.git > /dev/null 2>&1 || exit 1
+cd /tmp/BuildingAllTheShit/Lilu
 echo "Building Debug version of Lilu."
-xcodebuild -configuration Debug > /dev/null 2>&1
+xcodebuild -configuration Debug > /dev/null 2>&1 || exit 1
 echo "Building Release version of Lilu."
-xcodebuild -configuration Release > /dev/null 2>&1
-cd /tmp/BuildingAllTheShit
-echo "Copying Debug version of Lilu into AppleALC, VirutalSMC and WhateverGreen repos!"
-cp -r Lilu/build/Debug/Lilu.kext AppleALC/ 
-cp -r Lilu/build/Debug/Lilu.kext VirtualSMC/
-cp -r Lilu/build/Debug/Lilu.kext WhateverGreen/
-cd AppleALC
+xcodebuild -configuration Release > /dev/null 2>&1 || exit 1
+cd /tmp/BuildingAllTheShit/AppleALC
+echo "Copying Debug version of Lilu into AppleALC, VirutalSMC and WhateverGreen in order to compile them!"
+cp -r /tmp/BuildingAllTheShit/Lilu/build/Debug/Lilu.kext /tmp/BuildingAllTheShit/AppleALC/  || exit 1
+cp -r /tmp/BuildingAllTheShit/Lilu/build/Debug/Lilu.kext /tmp/BuildingAllTheShit/VirtualSMC/ || exit 1
+cp -r /tmp/BuildingAllTheShit/Lilu/build/Debug/Lilu.kext /tmp/BuildingAllTheShit/WhateverGreen/ || exit 1
+cd /tmp/BuildingAllTheShit/AppleALC
 echo "Building Release version of AppleALC."
-xcodebuild -configuration Release > /dev/null 2>&1
-cd /tmp/BuildingAllTheShit
-cd VirtualSMC
+xcodebuild -configuration Release > /dev/null 2>&1 || exit 1
+cd /tmp/BuildingAllTheShit/VirtualSMC
 echo "Building Release version of VirtualSMC."
-xcodebuild -configuration Release > /dev/null 2>&1
-cd /tmp/BuildingAllTheShit
-cd WhateverGreen
+xcodebuild -configuration Release > /dev/null 2>&1 || exit 1
+cd /tmp/BuildingAllTheShit/WhateverGreen
 echo "Building Release version of WhateverGreen."
-xcodebuild -configuration Release > /dev/null 2>&1
-cd /tmp/BuildingAllTheShit
-cd OpenCorePkg
-echo "Building latest build of Opencore."
-./macbuild.tool > /dev/null 2>&1
-cd /tmp/BuildingAllTheShit
-cd AptioFixPkg
-echo "Building latest build of AptioFixPkg."
-./macbuild.tool > /dev/null 2>&1
-cd /tmp/BuildingAllTheShit
-cd AppleSupportPkg
-echo "Building latest build of AppleSupportPkg."
-./macbuild.tool > /dev/null 2>&1
+xcodebuild -configuration Release > /dev/null 2>&1 || exit 1
+cd /tmp/BuildingAllTheShit/OpenCorePkg
+echo "Building latest commit of Opencore."
+./macbuild.tool > /dev/null 2>&1 || exit 1
+cd /tmp/BuildingAllTheShit/AptioFixPkg
+echo "Building latest commit of AptioFixPkg."
+./macbuild.tool > /dev/null 2>&1 || exit 1
+cd /tmp/BuildingAllTheShit/AppleSupportPkg
+echo "Building latest commit of AppleSupportPkg."
+./macbuild.tool > /dev/null 2>&1 || exit 1
 cd /tmp/BuildingAllTheShit
 echo "Creating Opencore EFI structure."
-mkdir ~/Desktop/CompletedBuilds
-mkdir ~/Desktop/CompletedBuilds/EFI
-mkdir ~/Desktop/CompletedBuilds/EFI/BOOT
-mkdir ~/Desktop/CompletedBuilds/EFI/OC
-mkdir ~/Desktop/CompletedBuilds/EFI/OC/ACPI
-mkdir ~/Desktop/CompletedBuilds/EFI/OC/Drivers
-mkdir ~/Desktop/CompletedBuilds/EFI/OC/Kexts
-mkdir ~/Desktop/CompletedBuilds/EFI/OC/Tools
-mkdir ~/Desktop/CompletedBuilds/Documents
-cp -r /tmp/BuildingAllTheShit/Lilu/build/Release/Lilu.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/AppleALC/build/Release/AppleALC.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/VirtualSMC/build/Release/package/Kexts/*.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/WhateverGreen/build/Release/WhateverGreen.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/VirtualSMC/build/Release/package/Drivers/VirtualSmc.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/AptioInputFix.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/AptioMemoryFix.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/CleanNvram.efi ~/Desktop/CompletedBuilds/EFI/OC/Tools > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/VerifyMsrE2.efi ~/Desktop/CompletedBuilds/EFI/OC/Tools > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/AppleSupportPkg/Binaries/RELEASE/*.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Binaries/RELEASE/OpenCore.efi ~/Desktop/CompletedBuilds/EFI/OC > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Binaries/RELEASE/BOOTx64.efi ~/Desktop/CompletedBuilds/EFI/BOOT > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Docs/Configuration.pdf ~/Desktop/CompletedBuilds/Documents > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Docs/SampleFull.plist ~/Desktop/CompletedBuilds/EFI/OC/Sample_config.plist > /dev/null 2>&1
-cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Docs/AcpiSamples/*.dsl ~/Desktop/CompletedBuilds/EFI/OC/ACPI > /dev/null 2>&1
+
+if [ -d "~/Desktop/CompletedBuilds" ]; then
+    echo "Removing current CompletedBuilds folder."
+    rm -rf ~/Desktop/CompletedBuilds || exit 1
+    mkdir ~/Desktop/CompletedBuilds
+    mkdir ~/Desktop/CompletedBuilds/EFI
+    mkdir ~/Desktop/CompletedBuilds/EFI/BOOT
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/ACPI
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/Drivers
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/Kexts
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/Tools
+    mkdir ~/Desktop/CompletedBuilds/Documents
+else
+    mkdir ~/Desktop/CompletedBuilds
+    mkdir ~/Desktop/CompletedBuilds/EFI
+    mkdir ~/Desktop/CompletedBuilds/EFI/BOOT
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/ACPI
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/Drivers
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/Kexts
+    mkdir ~/Desktop/CompletedBuilds/EFI/OC/Tools
+    mkdir ~/Desktop/CompletedBuilds/Documents
+fi
+
+cp -r /tmp/BuildingAllTheShit/Lilu/build/Release/Lilu.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/AppleALC/build/Release/AppleALC.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/VirtualSMC/build/Release/package/Kexts/*.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/WhateverGreen/build/Release/WhateverGreen.kext ~/Desktop/CompletedBuilds/EFI/OC/Kexts > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/VirtualSMC/build/Release/package/Drivers/VirtualSmc.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/AptioInputFix.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/AptioMemoryFix.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/CleanNvram.efi ~/Desktop/CompletedBuilds/EFI/OC/Tools > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/AptioFixPkg/Binaries/RELEASE/VerifyMsrE2.efi ~/Desktop/CompletedBuilds/EFI/OC/Tools > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/AppleSupportPkg/Binaries/RELEASE/*.efi ~/Desktop/CompletedBuilds/EFI/OC/Drivers > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Binaries/RELEASE/OpenCore.efi ~/Desktop/CompletedBuilds/EFI/OC > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Binaries/RELEASE/BOOTx64.efi ~/Desktop/CompletedBuilds/EFI/BOOT > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Docs/Configuration.pdf ~/Desktop/CompletedBuilds/Documents > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Docs/SampleFull.plist ~/Desktop/CompletedBuilds/EFI/OC/Sample_config.plist > /dev/null 2>&1 || exit 1
+cp -r /tmp/BuildingAllTheShit/OpenCorePkg/Docs/AcpiSamples/*.dsl ~/Desktop/CompletedBuilds/EFI/OC/ACPI > /dev/null 2>&1 || exit 1
 echo "Cleaning Up"
-rm -rf /tmp/BuildingAllTheShit > /dev/null 2>&1
+rm -rf /tmp/BuildingAllTheShit > /dev/null 2>&1 || exit 1
 echo "All Done!"
